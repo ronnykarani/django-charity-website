40fa23f4fe00e17374884ffab034383788945d15
ronnykarani <karanironny25@gmail.com>
2022-03-27 17:42:58 +0300

    about page

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    
+    path('', views.post_list, name='blog'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -9,5 +10,26 @@ def home(request):
     context = {'posts': posts, }
     return render(request, 'home.html', context)
 
+
 def contact(request):
-    return render(request, 'contact.html', {})
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+
+        message = 'Name: ' + message_name + '\n' + 'Email: ' + message_email + '\n' + 'Message: ' + message
+
+        # send an email
+        send_mail(
+            message_name, # subject
+            message, # message
+            message_email, # from email
+            [''], # to email
+        )
+        return render(request, 'contact.html', {'message_name': message_name, 'message_email': message_email, 'message': message})
+    else:
+        return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
