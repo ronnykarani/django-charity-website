6781bbf1af884cc45e73ade4a19c54da470268f5
ronnykarani <karanironny25@gmail.com>
2022-06-08 17:03:54 +0300

    team

--- gallery/models.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.db import models
-from django.utils import timezone
-
-
-# Create your models here.
-class Feed(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='gallery')
